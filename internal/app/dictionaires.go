4ce8c6512334c90a83488eea0f7f9948391b3c88
PaulioRandall <PaulioSirvero@users.noreply.github.com>
2019-03-10 14:14:14 +0000

    Modify dictionaries to return all tag dictionary entries

--- internal/app/dictionaires.go
+++ internal/app/dictionaires.go
@@ -15,17 +15,39 @@ type TagEntry struct {
 	Additional  string `json:"additional"`
 }
 
-func DictionaryHandler(w http.ResponseWriter, r *http.Request) {
+type DictionaryResponse struct {
+	Tags []TagEntry `json:"tags"`
+}
 
-	item := TagEntry{
-		Title:       "Low priority",
-		Description: "Low priority work items.",
-		Tag_id:      "low",
-		Additional:  "colour: #0000FF",
+func DictionaryHandler(w http.ResponseWriter, r *http.Request) {
+	response := DictionaryResponse{
+		Tags: createTags(),
 	}
 
 	shr.AppendStdHeaders(w)
-
-	json.NewEncoder(w).Encode(item)
+	json.NewEncoder(w).Encode(response)
 	log.Println(r.Host)
+}
+
+func createTags() []TagEntry {
+	return []TagEntry{
+		TagEntry{
+			Title:       "Low priority",
+			Description: "Low priority work items.",
+			Tag_id:      "low",
+			Additional:  "colour: #0000FF",
+		},
+		TagEntry{
+			Title:       "Mid priority",
+			Description: "Mid priority work items.",
+			Tag_id:      "mid",
+			Additional:  "colour: #00FF00",
+		},
+		TagEntry{
+			Title:       "High priority",
+			Description: "High priority work items.",
+			Tag_id:      "high",
+			Additional:  "colour: #FF0000",
+		},
+	}
 }
