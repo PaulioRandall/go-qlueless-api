065ad5eb0c4f904acf3fa0fe3aa73a40318238f3
Paulio <29413800+PaulioRandall@users.noreply.github.com>
2019-03-29 11:07:12 +0000

    Add HEAD and OPTIONS API tests for /openapi and /changelog

--- test/api/changelog_test.go
+++ test/api/changelog_test.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	. "github.com/PaulioRandall/go-qlueless-assembly-api/internal/pkg/asserts"
 )
@@ -40,5 +42,81 @@ func TestGET_Changelog(t *testing.T) {
 		"Access-Control-Allow-Methods": CORS_METHODS_PATTERN,
 	})
 
-	assert.NotEmpty(t, res.Body)
+	body, err := ioutil.ReadAll(res.Body)
+	require.Nil(t, err)
+	assert.NotEmpty(t, body)
+}
+
+func TestHEAD_Changelog(t *testing.T) {
+	t.Log(`Given a loaded changelog
+		When the changelog is requested
+		AND the HTTP method is 'HEAD'
+		Then ensure the response code is 200
+		And the 'Content-Type' header contains 'text/markdown'
+		And 'Access-Control-Allow-Origin' is '*'
+		And 'Access-Control-Allow-Headers' is '*'
+		And 'Access-Control-Allow-Methods' only contains GET, HEAD, and OPTIONS
+		And there is NO response body
+		...`)
+
+	req := APICall{
+		URL:    "http://localhost:8080/changelog",
+		Method: "HEAD",
+	}
+	res := req.fire()
+	defer res.Body.Close()
+
+	RequireStatusCode(t, 200, res)
+	AssertHeadersEquals(t, res.Header, map[string]string{
+		"Access-Control-Allow-Origin":  "*",
+		"Access-Control-Allow-Headers": "*",
+	})
+	AssertHeadersContains(t, res.Header, map[string][]string{
+		"Content-Type":                 []string{"text/markdown"},
+		"Access-Control-Allow-Methods": []string{"GET", "HEAD", "OPTIONS"},
+	})
+	AssertHeadersMatches(t, res.Header, map[string]string{
+		"Access-Control-Allow-Methods": CORS_METHODS_PATTERN,
+	})
+
+	body, err := ioutil.ReadAll(res.Body)
+	require.Nil(t, err)
+	assert.Empty(t, body)
+}
+
+func TestOPTIONS_Changelog(t *testing.T) {
+	t.Log(`Given a loaded changelog
+		When the changelog is requested
+		AND the HTTP method is 'OPTIONS'
+		Then ensure the response code is 200
+		And the 'Content-Type' header contains 'text/markdown'
+		And 'Access-Control-Allow-Origin' is '*'
+		And 'Access-Control-Allow-Headers' is '*'
+		And 'Access-Control-Allow-Methods' only contains GET, HEAD, and OPTIONS
+		And there is NO response body
+		...`)
+
+	req := APICall{
+		URL:    "http://localhost:8080/changelog",
+		Method: "OPTIONS",
+	}
+	res := req.fire()
+	defer res.Body.Close()
+
+	RequireStatusCode(t, 200, res)
+	AssertHeadersEquals(t, res.Header, map[string]string{
+		"Access-Control-Allow-Origin":  "*",
+		"Access-Control-Allow-Headers": "*",
+	})
+	AssertHeadersContains(t, res.Header, map[string][]string{
+		"Content-Type":                 []string{"text/markdown"},
+		"Access-Control-Allow-Methods": []string{"GET", "HEAD", "OPTIONS"},
+	})
+	AssertHeadersMatches(t, res.Header, map[string]string{
+		"Access-Control-Allow-Methods": CORS_METHODS_PATTERN,
+	})
+
+	body, err := ioutil.ReadAll(res.Body)
+	require.Nil(t, err)
+	assert.Empty(t, body)
 }
--- test/api/openapi_test.go
+++ test/api/openapi_test.go
@@ -2,8 +2,10 @@ package api
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	. "github.com/PaulioRandall/go-qlueless-assembly-api/internal/pkg/asserts"
@@ -44,4 +46,78 @@ func TestGET_OpenAPI(t *testing.T) {
 	var spec map[string]interface{}
 	err := json.NewDecoder(res.Body).Decode(&spec)
 	require.Nil(t, err)
+}
+
+func TestHEAD_OpenAPI(t *testing.T) {
+	t.Log(`Given a loaded OpenAPI specification
+		When the specification is requested
+		AND the HTTP method is 'HEAD'
+		Then ensure the response code is 200
+		And the 'Content-Type' header contains 'application/vnd.oai.openapi+json'
+		And 'Access-Control-Allow-Origin' is '*'
+		And 'Access-Control-Allow-Headers' is '*'
+		And 'Access-Control-Allow-Methods' only contains GET, HEAD, and OPTIONS
+		And there is NO response body
+		...`)
+
+	req := APICall{
+		URL:    "http://localhost:8080/openapi",
+		Method: "HEAD",
+	}
+	res := req.fire()
+	defer res.Body.Close()
+
+	RequireStatusCode(t, 200, res)
+	AssertHeadersEquals(t, res.Header, map[string]string{
+		"Access-Control-Allow-Origin":  "*",
+		"Access-Control-Allow-Headers": "*",
+	})
+	AssertHeadersContains(t, res.Header, map[string][]string{
+		"Content-Type":                 []string{"application/vnd.oai.openapi+json"},
+		"Access-Control-Allow-Methods": []string{"GET", "HEAD", "OPTIONS"},
+	})
+	AssertHeadersMatches(t, res.Header, map[string]string{
+		"Access-Control-Allow-Methods": CORS_METHODS_PATTERN,
+	})
+
+	body, err := ioutil.ReadAll(res.Body)
+	require.Nil(t, err)
+	assert.Empty(t, body)
+}
+
+func TestOPTIONS_OpenAPI(t *testing.T) {
+	t.Log(`Given a loaded OpenAPI specification
+		When the specification is requested
+		AND the HTTP method is 'OPTIONS'
+		Then ensure the response code is 200
+		And the 'Content-Type' header contains 'application/vnd.oai.openapi+json'
+		And 'Access-Control-Allow-Origin' is '*'
+		And 'Access-Control-Allow-Headers' is '*'
+		And 'Access-Control-Allow-Methods' only contains GET, HEAD, and OPTIONS
+		And there is NO response body
+		...`)
+
+	req := APICall{
+		URL:    "http://localhost:8080/openapi",
+		Method: "OPTIONS",
+	}
+	res := req.fire()
+	defer res.Body.Close()
+
+	RequireStatusCode(t, 200, res)
+	AssertHeadersEquals(t, res.Header, map[string]string{
+		"Access-Control-Allow-Origin":  "*",
+		"Access-Control-Allow-Headers": "*",
+	})
+	AssertHeadersContains(t, res.Header, map[string][]string{
+		"Content-Type":                 []string{"application/vnd.oai.openapi+json"},
+		"Access-Control-Allow-Methods": []string{"GET", "HEAD", "OPTIONS"},
+	})
+	AssertHeadersMatches(t, res.Header, map[string]string{
+		"Access-Control-Allow-Methods": CORS_METHODS_PATTERN,
+	})
+
+	body, err := ioutil.ReadAll(res.Body)
+	require.Nil(t, err)
+	assert.Empty(t, body)
 }
