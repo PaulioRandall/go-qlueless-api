2a9ddb88d28f6cffbb46d773768cf647254f1d1f
Paulio <29413800+PaulioRandall@users.noreply.github.com>
2019-04-01 22:14:41 +0100

    Add docs to changelog pkg

--- internal/app/changelog/changelog.go
+++ internal/app/changelog/changelog.go
@@ -1,57 +1,18 @@
+// Package changelog provides handlers for fetching the API changelog for the
+// API.
+//
+// Changelogs for an API provide a way for consumers to see what changes have
+// been made to it over time. Particularly useful for client developers during
+// preventative maintenance and upgrades to check for fixes and new features
+// so they can decide whether to make changes their end.
+//
+// Regardless of the actual versioning approach used, versioning is key to a
+// good changelog. Sensible client developers will document the version they
+// are targetting and expect to easily find all changes since then in the
+// API changelog.
+//
+// I've used a changelog here to show the changes to the API itself, not the
+// source code.
+//
+// Keep a Changelog: https://keepachangelog.com/en/1.0.0/
 package changelog
-
-import (
-	"io/ioutil"
-	"log"
-	"net/http"
-
-	. "github.com/PaulioRandall/go-qlueless-assembly-api/internal/pkg"
-)
-
-const mime_md = "text/markdown; charset=utf-8"
-
-var changelog *[]byte = nil
-
-// ChangelogHandler handles requests for the APIs changelog
-func ChangelogHandler(res http.ResponseWriter, req *http.Request) {
-	LogRequest(req)
-	AppendCORSHeaders(&res, "GET, HEAD, OPTIONS")
-
-	switch req.Method {
-	case "GET":
-		get_Changelog(&res, req)
-	case "HEAD":
-		fallthrough
-	case "OPTIONS":
-		res.Header().Set("Content-Type", mime_md)
-		res.WriteHeader(http.StatusOK)
-	default:
-		MethodNotAllowed(&res, req)
-	}
-}
-
-// get_Changelog generates responses for obtaining the CHANGELOG
-func get_Changelog(res *http.ResponseWriter, req *http.Request) {
-	if changelog == nil {
-		log.Println("[BUG] CHANGELOG not loaded")
-		WriteServerError(res, req)
-		return
-	}
-
-	(*res).Header().Set("Content-Type", mime_md)
-	(*res).WriteHeader(http.StatusOK)
-	(*res).Write(*changelog)
-}
-
-// LoadChangelog loads the changelog from a file
-func LoadChangelog() {
-
-	path := "./CHANGELOG.md"
-	bytes, err := ioutil.ReadFile(path)
-	if LogIfErr(err) {
-		changelog = nil
-		return
-	}
-
-	changelog = &bytes
-}
---
+++ internal/app/changelog/handlers.go
@@ -0,0 +1,57 @@
+package changelog
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	. "github.com/PaulioRandall/go-qlueless-assembly-api/internal/pkg"
+)
+
+const mime_md = "text/markdown; charset=utf-8"
+
+var changelog *[]byte = nil
+
+// ChangelogHandler handles requests for the APIs changelog
+func ChangelogHandler(res http.ResponseWriter, req *http.Request) {
+	LogRequest(req)
+	AppendCORSHeaders(&res, "GET, HEAD, OPTIONS")
+
+	switch req.Method {
+	case "GET":
+		_GET_Changelog(&res, req)
+	case "HEAD":
+		fallthrough
+	case "OPTIONS":
+		res.Header().Set("Content-Type", mime_md)
+		res.WriteHeader(http.StatusOK)
+	default:
+		MethodNotAllowed(&res, req)
+	}
+}
+
+// _GET_Changelog generates responses for obtaining the CHANGELOG
+func _GET_Changelog(res *http.ResponseWriter, req *http.Request) {
+	if changelog == nil {
+		log.Println("[BUG] CHANGELOG not loaded")
+		WriteServerError(res, req)
+		return
+	}
+
+	(*res).Header().Set("Content-Type", mime_md)
+	(*res).WriteHeader(http.StatusOK)
+	(*res).Write(*changelog)
+}
+
+// LoadChangelog loads the changelog from a file
+func LoadChangelog() {
+
+	path := "./CHANGELOG.md"
+	bytes, err := ioutil.ReadFile(path)
+	if LogIfErr(err) {
+		changelog = nil
+		return
+	}
+
+	changelog = &bytes
+}
