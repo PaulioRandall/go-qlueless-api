f0ae9a2cda692d6ae3c563ab111b8eae5d4e16a4
Paulio <29413800+PaulioRandall@users.noreply.github.com>
2019-04-01 22:30:02 +0100

    Document functions in {root}/internal/pkg/ventures/assert.go

--- internal/pkg/ventures/asserts.go
+++ internal/pkg/ventures/asserts.go
@@ -11,6 +11,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// AssertVentureFromReader asserts that a Venture decoded from an io.Reader
+// has the required fields populated and in the correct format
 func AssertVentureFromReader(t *testing.T, r io.Reader) Venture {
 	v, err := DecodeVenture(r)
 	require.Nil(t, err)
@@ -18,6 +20,8 @@ func AssertVentureFromReader(t *testing.T, r io.Reader) Venture {
 	return v
 }
 
+// AssertVentureFromReader asserts that a Venture has the required fields
+// populated and in the correct format
 func AssertGenericVenture(t *testing.T, v Venture) {
 	assert.NotEmpty(t, v.ID, "Venture.ID")
 	assert.NotEmpty(t, v.Description, "Venture.Description")
@@ -27,6 +31,8 @@ func AssertGenericVenture(t *testing.T, v Venture) {
 	}
 }
 
+// AssertVentureSliceFromReader asserts that a Venture slice decoded from an
+// io.Reader has the required fields populated and in the correct format
 func AssertVentureSliceFromReader(t *testing.T, r io.Reader) []Venture {
 	v, err := DecodeVentureSlice(r)
 	require.Nil(t, err)
@@ -34,15 +40,21 @@ func AssertVentureSliceFromReader(t *testing.T, r io.Reader) []Venture {
 	return v
 }
 
+// AssertGenericVentureSlice asserts that a Venture slice has the required
+// fields populated and in the correct format
 func AssertGenericVentureSlice(t *testing.T, vens []Venture) {
 	for _, v := range vens {
 		AssertGenericVenture(t, v)
 	}
 }
 
+// AssertWrappedVentureSliceFromReader asserts that a Venture slice wrapped
+// within a WrappedReply and decoded from an io.Reader has the required fields
+// populated and in the correct format
 func AssertWrappedVentureSliceFromReader(t *testing.T, r io.Reader) (w.WrappedReply, []Venture) {
 	wr, err := w.DecodeWrappedReplyFromReader(r)
 	require.Nil(t, err)
+	w.AssertWrappedReply(t, wr)
 
 	var v []Venture
 	config := ms.DecoderConfig{
@@ -60,9 +72,13 @@ func AssertWrappedVentureSliceFromReader(t *testing.T, r io.Reader) (w.WrappedRe
 	return wr, v
 }
 
+// AssertWrappedVentureFromReader asserts that a Venture wrapped within a
+// WrappedReply and decoded from an io.Reader has the required fields populated
+// and in the correct format
 func AssertWrappedVentureFromReader(t *testing.T, r io.Reader) (w.WrappedReply, Venture) {
 	wr, err := w.DecodeWrappedReplyFromReader(r)
 	require.Nil(t, err)
+	w.AssertWrappedReply(t, wr)
 
 	var v Venture
 	config := ms.DecoderConfig{
